e4d45b8422c630ade8030dc3e49c4736e6e6ac6b
Maxim Voloshin <maksym.voloshyn@bazaarvoice.com>
2020-06-20 15:09:15 +0300

    release 0.1.0

---
+++ precache-manifest.98679342b5f02a809bbfe1264e8fcc71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f408fda4859305305b05dde756fe5a20",
+    "url": "/day-planner/index.html"
+  },
+  {
+    "revision": "ca9058feaf1450cc1778",
+    "url": "/day-planner/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6213f8a452705bb8556d",
+    "url": "/day-planner/static/js/2.2969c82f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/day-planner/static/js/2.2969c82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca9058feaf1450cc1778",
+    "url": "/day-planner/static/js/main.01bdf329.chunk.js"
+  },
+  {
+    "revision": "1ade38255a745529bb9b",
+    "url": "/day-planner/static/js/runtime-main.5ac9428c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/day-planner/static/media/logo.5d5d9eef.svg"
+  }
+]);
